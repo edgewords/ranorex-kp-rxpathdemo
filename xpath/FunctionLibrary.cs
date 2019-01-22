a69a60470e77856fb7abc47e4dde40f7caab55fe
Steve Powell <steve.powell@edgewordstraining.co.uk>
2019-01-22 12:53:22 +0000

    Initial commit

---
+++ xpath/FunctionLibrary.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Created by Ranorex
+ * User: edgewords
+ * Date: 17/01/2019
+ * Time: 22:16
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Threading;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Testing;
+
+namespace xpath
+{
+    /// <summary>
+    /// Ranorex user code collection. A collection is used to publish user code methods to the user code library.
+    /// </summary>
+    [UserCodeCollection]
+    public class FunctionLibrary
+    {
+        // You can use the "Insert New User Code Method" functionality from the context menu,
+        // to add a new method with the attribute [UserCodeMethod].
+        
+        
+        /// <summary>
+        /// This is a placeholder text. Please describe the purpose of the
+        /// user code method here. The method is published to the user code library
+        /// within a user code collection.
+        /// </summary>
+        [UserCodeMethod]
+        public static void MyMethod()
+        {
+        	
+        }
+    }
+}
